6706631af1255623e2bddfa012514e438fb963a6
robicse11127 <robizmailbox@gmail.com>
2023-10-07 01:47:09 +0600

    Episode 5 done

--- functions.php
+++ functions.php
@@ -30,12 +30,16 @@ function cristina_public_assets() {
 
 	// Regsiter styles.
 	wp_register_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', [], wp_rand(), 'all' );
+	wp_register_style( 'cristina-main', get_template_directory_uri() . '/assets/css/main.css', [ 'bootstrap' ], wp_rand(), 'all' );
 
 	// Register scritps.
 	wp_register_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', [], wp_rand(), true );
+	wp_register_script( 'cristina-main', get_template_directory_uri() . '/assets/js/main.js', [], wp_rand(), true );
 
 	// Enqueue scritps.
 	wp_enqueue_style( 'bootstrap' );
+	wp_enqueue_style( 'cristina-main' );
 	wp_enqueue_script( 'bootstrap' );
+	wp_enqueue_script( 'cristina-main' );
 }
 add_action( 'wp_enqueue_scripts', 'cristina_public_assets' );
