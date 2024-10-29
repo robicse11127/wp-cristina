a77dfdc5b133cbc8fb12d6b55fd74518c8e5ac61
robicse11127 <robizmailbox@gmail.com>
2024-10-30 01:08:31 +0600

    Episode 18 done

--- inc/class-woo-hooks.php
+++ inc/class-woo-hooks.php
@@ -11,6 +11,8 @@ class Cristina_Woo_Hooks {
 		remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
 		remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 		remove_action( 'woocommerce_bafter_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+		// remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
+		// remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
 
 		add_action( 'woocommerce_before_main_content', [ $this, 'cristina_open_content_wrapper' ], 10 );
 		add_action( 'woocommerce_after_main_content', [ $this, 'cristina_close_content_wrapper' ], 10 );
@@ -19,6 +21,7 @@ class Cristina_Woo_Hooks {
 
 		// add_filter( 'woocommerce_show_page_title', '__return_false' );
 		add_filter( 'woocommerce_show_page_title', [ $this, 'cristina_remove_shop_title' ], 10 );
+		// add_filter( 'woocommerce_catalog_orderby', [ $this, 'cristina_catalog_orderby' ], 10 );
 	}
 
 	public function crtistina_setup_theme() {
@@ -57,6 +60,18 @@ class Cristina_Woo_Hooks {
 		?>
 		</div>
 		<?php
+	}
+
+	public function cristina_catalog_orderby( $sortby ) {
+		unset( $sortby['popularity'] );
+		unset( $sortby['rating'] );
+		unset( $sortby['date'] );
+		unset( $sortby['price'] );
+
+		$sortby['price-desc'] = 'Custom Price: High to Low';
+		$sortby['price-asc'] = 'Custom Price: Low to High';
+
+		return $sortby;
 	}
 
 }
